b60fc1d626d259bd84053b46ea97d75ac77586e0
ol <and-new@rambler.ru>
2017-05-07 18:06:43 +0600

    Save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,16 @@ class ContactsController < ApplicationController
   def index
   end
 
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+  
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
+
 end
+
