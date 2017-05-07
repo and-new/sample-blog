f3337de2d405b12fcbda4311309e0be123ae9297
ol <and-new@rambler.ru>
2017-05-07 18:53:00 +0600

    Change /contacts/new

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
    get '/' => 'home#index'
  # get 'home/index'
   
-  resources :contacts, only: [:new, :create]
+# get 'contacts' => 'contacts#new'
+  resources :contacts, only: [:new, :create], path_names: { :new => '' }
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
